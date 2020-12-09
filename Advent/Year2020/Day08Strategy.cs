cba4aef1c2e074396cb541549e9adb13a647de99
Mark Haines <lilserf@gmail.com>
2020-12-08 21:38:41 -0500

    Day 8

---
+++ Advent/Year2020/Day08Strategy.cs
@@ -0,0 +1,65 @@
+﻿using Advent;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2020.Year2020
+{
+	class Day08Strategy : DayStrategyBasic
+	{
+
+		IEnumerable<string> m_lines;
+
+		public Day08Strategy(string file)
+		{
+			m_lines = File.ReadAllLines(file);
+		}
+
+
+		public override string Part1()
+		{
+			Handheld handheld = new Handheld(m_lines);
+
+			HashSet<int> executed = new HashSet<int>();
+			executed.Add(0);
+
+			while(true)
+			{
+				(int newPc, long accum) = handheld.Step();
+				if (executed.Contains(newPc))
+				{
+					return accum.ToString();
+				}
+				executed.Add(newPc);
+			}
+
+		}
+
+		public override string Part2()
+		{
+			int index = 0;
+
+			while(true)
+			{
+				var lines = m_lines.ToList();
+				while(!lines[index].StartsWith("jmp"))
+				{
+					index++;
+				}
+				lines[index] = lines[index].Replace("jmp", "nop");
+
+				Handheld handheld = new Handheld(lines);
+				(int err, long acc) = handheld.Run(500);
+
+				if (err == 0)
+					return acc.ToString();
+				else
+					index++;
+			}
+
+		}
+	}
+}
---
+++ Advent/Year2020/Handheld.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2020.Year2020
+{
+	enum Operation
+	{
+		Acc,
+		Jmp,
+		Nop
+	}
+
+	class Instruction
+	{
+		public Operation Operation;
+		public int Argument;
+
+		public override string ToString()
+		{
+			return $"{Operation} {Argument}";
+		}
+	}
+
+	class Handheld
+	{
+		List<Instruction> m_data = new List<Instruction>();
+
+		int m_pc = 0;
+		long m_accumulator = 0;
+
+		public Handheld(IEnumerable<string> instructions)
+		{
+			foreach(var s in instructions)
+			{
+				var splits = s.Split(' ');
+
+				Instruction newIn = new Instruction();
+				newIn.Argument = int.Parse(splits[1]);
+				switch(splits[0])
+				{
+					case "acc":
+						newIn.Operation = Operation.Acc;
+						break;
+					case "nop":
+						newIn.Operation = Operation.Nop;
+						break;
+					case "jmp":
+						newIn.Operation = Operation.Jmp;
+						break;
+
+				}
+				m_data.Add(newIn);
+			}
+		}
+
+		public (int, long) Run(int timeoutMs)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			while(m_pc < m_data.Count())
+			{
+				Step(false);
+				if (sw.ElapsedMilliseconds > timeoutMs)
+					return (1, m_accumulator);
+			}
+
+			return (0, m_accumulator);
+		}
+
+		public (int, long) Step(bool print = true)
+		{
+			var inst = m_data.ElementAt(m_pc);
+			if(print) Console.WriteLine($"Executing instruction {m_pc} - Accum is {m_accumulator}\n  {inst}");
+
+			if(inst.Operation == Operation.Acc)
+			{
+				m_accumulator += inst.Argument;
+				m_pc++;
+			}
+			else if(inst.Operation == Operation.Jmp)
+			{
+				m_pc += inst.Argument;
+			}
+			else if(inst.Operation == Operation.Nop)
+			{
+				m_pc++;
+			}
+
+			return (m_pc, m_accumulator);
+		}
+
+	}
+}
