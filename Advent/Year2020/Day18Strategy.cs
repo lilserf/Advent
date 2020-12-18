c96d79e68271e14b2415f88eee6fb34c76b86ac9
Mark Haines <lilserf@gmail.com>
2020-12-18 15:46:13 -0500

    Day 18 Part 2

--- Advent/Year2020/Day18Strategy.cs
+++ Advent/Year2020/Day18Strategy.cs
@@ -14,6 +14,30 @@ namespace Advent2020.Year2020
 		public abstract bool CanFill();
 	}
 
+	class PendingExpr : Expr
+	{
+		public string Text { get; set; }
+		public PendingExpr(string text)
+		{
+			Text = text;
+		}
+
+		public override long Evaluate()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Fill(Expr ex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool CanFill()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
 	class ConstantExpr : Expr
 	{
 		long m_value;
@@ -129,7 +153,7 @@ namespace Advent2020.Year2020
 
 		List<Expr> m_exprs = new List<Expr>();
 
-		private Expr ParseExpr(string line, bool partB)
+		private Expr ParseExpr(string line)
 		{
 			Stack<Expr> exprStack = new Stack<Expr>();
 			exprStack.Push(new GroupExpr());
@@ -200,9 +224,86 @@ namespace Advent2020.Year2020
 			return m_exprs.Sum(x => x.Evaluate()).ToString();
 		}
 
+		private Expr ParseAddExpr(string line)
+		{
+			OperatorExpr add = new OperatorExpr('+');
+			add.Left = new ConstantExpr(0);
+			var splits = line.Split('+');
+			foreach(var num in splits)
+			{
+				int val = int.Parse(num);
+				add.Fill(new ConstantExpr(val));
+				OperatorExpr newAdd = new OperatorExpr('+');
+				newAdd.Left = add;
+				add = newAdd;
+			}
+
+			add.Right = new ConstantExpr(0);
+			return add;
+		}
+
+		private Expr ParseSubExpr(string line)
+		{
+			var splits = line.Split('*');
+			OperatorExpr mult = new OperatorExpr('*');
+			mult.Left = new ConstantExpr(1);
+
+			foreach(var ex in splits)
+			{
+				mult.Fill(ParseAddExpr(ex));
+				var newMult = new OperatorExpr('*');
+				newMult.Left = mult;
+				mult = newMult;
+			}
+
+			mult.Right = new ConstantExpr(1);
+			return mult;
+		}
+
+		private long SolvePartB(string line)
+		{
+			Stack<string> exprStack = new Stack<string>();
+			exprStack.Push("");
+
+			foreach(char c in line)
+			{
+				if (c == ' ') continue;
+
+				if(c == '(')
+				{
+					exprStack.Push("");
+				}
+				else if(c == ')')
+				{
+					var top = exprStack.Pop();
+					Expr sub = ParseSubExpr(top);
+					long answer = sub.Evaluate();
+					top = exprStack.Pop();
+					top += answer.ToString();
+					exprStack.Push(top);
+				}
+				else
+				{
+					var top = exprStack.Pop();
+					top += c;
+					exprStack.Push(top);
+				}
+			}
+
+			var final = exprStack.Pop();
+			return ParseSubExpr(final).Evaluate();
+		}
+
+
 		public override string Part2()
 		{
-			return "";
+			Console.WriteLine(SolvePartB("1 + 2 * 3 + 4 * 5 + 6"));
+			Console.WriteLine(SolvePartB("1 + (2 * 3) + (4 * (5 + 6))"));
+			Console.WriteLine(SolvePartB("5 + (8 * 3 + 9 + 3 * 4 * 3)")); 
+			Console.WriteLine(SolvePartB("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")); 
+			Console.WriteLine(SolvePartB("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"));
+
+			return m_data.Sum(x => SolvePartB(x)).ToString();
 		}
 	}
 }
