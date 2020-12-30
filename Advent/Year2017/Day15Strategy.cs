3c0e253f315c05ab21408aac3894f68434e18a72
Mark Haines <lilserf@gmail.com>
2020-12-30 00:11:18 -0500

    2017 Day 15 and 16

---
+++ Advent/Year2017/Day15Strategy.cs
@@ -0,0 +1,84 @@
+﻿using Advent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2020.Year2017
+{
+	class Day15Strategy : DayLineLoaderBasic
+	{
+		public override void ParseInputLine(string line)
+		{
+			
+		}
+
+		long m_initialA = 591;
+		long m_initialB = 393;
+
+		long m_factorA = 16807;
+		long m_factorB = 48271;
+
+		long m_mod = 2147483647;
+		public override string Part1()
+		{
+
+			long aVal = m_initialA;
+			long bVal = m_initialB;
+
+			int count = 0;
+			for(long i=0; i < 40000000; i++)
+			{
+				aVal = (aVal * m_factorA) % m_mod;
+				bVal = (bVal * m_factorB) % m_mod;
+
+				if((aVal & 0xFFFF) == (bVal & 0xFFFF))
+				{
+					count++;
+				}
+			}
+
+			return count.ToString();
+		}
+
+		public override string Part2()
+		{
+			Queue<long> aQueue = new Queue<long>();
+			Queue<long> bQueue = new Queue<long>();
+
+			long aVal = m_initialA;
+			long bVal = m_initialB;
+
+			int pairCount = 0;
+			int count = 0;
+			while(pairCount < 5000000)
+			{
+				aVal = (aVal * m_factorA) % m_mod;
+				bVal = (bVal * m_factorB) % m_mod;
+
+				if(aVal % 4 == 0)
+				{
+					aQueue.Enqueue(aVal);
+				}
+				if(bVal % 8 == 0)
+				{
+					bQueue.Enqueue(bVal);
+				}
+
+				if (aQueue.Count >= 1 && bQueue.Count >= 1)
+				{
+					pairCount++;
+					long aPending = aQueue.Dequeue();
+					long bPending = bQueue.Dequeue();
+					if ((aPending & 0xFFFF) == (bPending & 0xFFFF))
+					{
+						count++;
+					}
+				}
+			}
+
+			return count.ToString();
+		}
+	}
+}
---
+++ Advent/Year2017/Day16Strategy.cs
@@ -0,0 +1,123 @@
+﻿using Advent;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Advent2020.Year2017
+{
+	class Day16Strategy : DayLineLoaderBasic
+	{
+		List<string> m_data = new List<string>();
+
+		public override void ParseInputLine(string line)
+		{
+			var splits = line.Split(',');
+			m_data.AddRange(splits);
+		}
+
+		char[] m_line = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p' };
+		int m_front = 0;
+
+		private void Swap(int i1, int i2)
+		{
+			int r1 = (i1 + m_front) % 16;
+			int r2 = (i2 + m_front) % 16;
+
+			char temp = m_line[r1];
+			m_line[r1] = m_line[r2];
+			m_line[r2] = temp;
+		}
+
+		private int Find(char c)
+		{
+			for(int i=0; i < 16; i++)
+			{
+				if(m_line[i] == c)
+				{
+					return (i + 16 - m_front) % 16;
+				}
+			}
+
+			return -1;
+		}
+
+		private string GetLine()
+		{
+			string s = "";
+			for (int i = 0; i < 16; i++)
+			{
+				int idx = (m_front + i) % 16;
+				s += m_line[idx];
+			}
+			return s;
+		}
+
+		private void DoDance()
+		{
+			foreach (var instr in m_data)
+			{
+				if (instr[0] == 's')
+				{
+					m_front = m_front + 16 - int.Parse(instr.Substring(1));
+					m_front %= 16;
+				}
+				else if (instr[0] == 'x')
+				{
+					var matches = Regex.Match(instr, @"x(\d+)/(\d+)");
+					if (matches.Success)
+					{
+						int i1 = int.Parse(matches.Groups[1].Value);
+						int i2 = int.Parse(matches.Groups[2].Value);
+
+						Swap(i1, i2);
+					}
+				}
+				else if (instr[0] == 'p')
+				{
+					char c1 = instr[1];
+					char c2 = instr[3];
+
+					Swap(Find(c1), Find(c2));
+				}
+				//Console.WriteLine($"{instr}:\t{GetLine()}");
+				//Console.ReadKey();
+			}
+		}
+
+		public override string Part1()
+		{
+			//Console.WriteLine($"Start:\t{GetLine()}");
+
+			DoDance();
+
+			return GetLine();
+		}
+
+
+		public override string Part2()
+		{
+			List<string> set = new List<string>();
+			set.Add("abcdefghijklmnop");
+
+			for(int i=0; i < 100000; i++)
+			{
+				var s = GetLine();
+				if(!set.Contains(s))
+				{
+					set.Add(s);
+				}
+				else
+				{
+					break;
+				}
+				DoDance();
+			}
+
+			return set.ElementAt(1000000000 % set.Count());
+		}
+	}
+}
