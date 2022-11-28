0596b325cd7daddf0d5850d571b2a63ea8f04cc1
Mark Haines <lilserf@gmail.com>
2022-11-28 00:41:17 -0500

    Converted to .NET 7 and started working on missed 2021 days

--- Advent/Vec3.cs
+++ Advent/Vec3.cs
@@ -36,6 +36,8 @@ namespace Advent
         public static readonly Vec3 ZNegLeft = new Vec3(-1, 0, -1);
         public static readonly Vec3 ZNegUpLeft = new Vec3(-1, -1, -1);
 
+        
+
         public static readonly Vec3[] Adjacent = new[]
         {
             Up, UpRight, Right, DownRight, Down, DownLeft, Left, UpLeft,
@@ -108,11 +110,92 @@ namespace Advent
         {
             return Math.Abs(b.X - a.X) + Math.Abs(b.Y - a.Y) + Math.Abs(b.Z - a.Z);
         }
+        public static double Distance(Vec3 a, Vec3 b)
+        {
+            return Math.Sqrt((b.X - a.X) * (b.X - a.X) + (b.Y - a.Y) * (b.Y - a.Y) + (b.Z - a.Z) * (b.Z - a.Z));
+        }
+
         public override string ToString()
         {
             return $"[{X},{Y},{Z}]";
         }
+        
+        public static Vec3 Parse(string s)
+        {
+            var split = s.Split(',');
+            return new Vec3(int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2]));
+        }
+
         public string DebuggerDisplay { get { return ToString(); } }
 
+    }
+
+    public struct Vec3Rotation
+    {
+        public static readonly Vec3Rotation ZRight = new Vec3Rotation(Dest.PositiveY, Dest.NegativeX, Dest.PositiveZ);
+        public static readonly Vec3Rotation ZLeft = new Vec3Rotation(Dest.NegativeY, Dest.PositiveX, Dest.PositiveZ);
+        public static readonly Vec3Rotation Z180 = new Vec3Rotation(Dest.NegativeX, Dest.NegativeY, Dest.PositiveZ);
+
+        public static readonly Vec3Rotation XRight = new Vec3Rotation(Dest.PositiveX, Dest.NegativeZ, Dest.PositiveY);
+        public static readonly Vec3Rotation XLeft = new Vec3Rotation(Dest.PositiveX, Dest.PositiveZ, Dest.NegativeY);
+        public static readonly Vec3Rotation X180 = new Vec3Rotation(Dest.PositiveX, Dest.NegativeY, Dest.NegativeZ);
+
+        public static readonly Vec3Rotation YRight = new Vec3Rotation(Dest.NegativeZ, Dest.PositiveY, Dest.PositiveX);
+        public static readonly Vec3Rotation YLeft = new Vec3Rotation(Dest.PositiveZ, Dest.PositiveY, Dest.NegativeX);
+        public static readonly Vec3Rotation Y180 = new Vec3Rotation(Dest.NegativeX, Dest.PositiveY, Dest.NegativeZ);
+
+        public static readonly Vec3Rotation None = new Vec3Rotation(Dest.PositiveX, Dest.PositiveY, Dest.PositiveZ);
+
+        public enum Dest
+        {
+            PositiveX,
+            NegativeX,
+            PositiveY,
+            NegativeY,
+            PositiveZ,
+            NegativeZ,
+        }
+
+        Dest m_x, m_y, m_z;
+
+        public Vec3Rotation(Dest x, Dest y, Dest z)
+        {
+            m_x = x;
+            m_y = y;
+            m_z = z;
+        }
+
+        private void ApplyOne(int src, Vec3 dest, Dest op)
+        {
+            switch(op)
+            {
+                case Dest.PositiveX: dest.X = src; break;
+                case Dest.NegativeX: dest.X = -src; break;
+                case Dest.PositiveY: dest.Y = src; break;
+                case Dest.NegativeY: dest.Y = -src; break;
+                case Dest.PositiveZ: dest.Z = src; break;
+                case Dest.NegativeZ: dest.Z = -src; break;
+                default: throw new InvalidOperationException();
+            }
+        }
+
+        public Vec3 Apply(Vec3 a)
+        {
+            Vec3 b = new Vec3();
+            ApplyOne(a.X, b, m_x);
+            ApplyOne(a.Y, b, m_y);
+            ApplyOne(a.Z, b, m_z);
+            return b;
+        }
+
+        public static Vec3Rotation operator*(Vec3Rotation a, Vec3Rotation b)
+        {
+
+        }
+
+        public static Vec3 operator*(Vec3 a, Vec3Rotation b)
+        {
+            return b.Apply(a);
+        }
     }
 }
---
+++ Advent/Year2021/Day19.cs
@@ -0,0 +1,150 @@
+﻿using Advent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2020.Year2021
+{
+    class Scanner
+    {
+        int m_id;
+
+        public IList<Vec3> Beacons => m_beacons;
+        List<Vec3> m_beacons;
+
+        public IEnumerable<double> Distances => m_distances;
+        List<double> m_distances;
+
+        public Vec3Rotation Rotation => Vec3Rotation.None;
+        public Vec3 Transform => Vec3.Zero;
+
+        public Scanner(int id)
+        {
+            m_id = id;
+            m_beacons= new List<Vec3>();
+        }
+
+        public void CalculateDistances()
+        {
+            m_distances = new List<double>();
+            foreach(var a in m_beacons)
+            {
+                foreach(var b in m_beacons)
+                {
+                    if (a == b) continue;
+
+                    m_distances.Add(Vec3.Distance(a, b));
+                }
+            }
+        }
+
+    }
+
+    internal class Day19 : DayLineLoaderBasic
+    {
+        List<Scanner> m_scanners = new();
+
+        int m_nextId = 0;
+        Scanner m_currScanner;
+
+        public override void ParseInputLine(string line)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                return;
+
+            if(line.StartsWith("---"))
+            {
+                if(m_currScanner != null)
+                {
+                    m_currScanner.CalculateDistances();
+                    m_scanners.Add(m_currScanner);
+                }
+                m_currScanner = new Scanner(m_nextId++);
+            }
+            else
+            {
+                m_currScanner.Beacons.Add(Vec3.Parse(line));
+            }
+        }
+
+        static Vec3Rotation[] Transforms =
+        {
+            Vec3Rotation.None,
+            Vec3Rotation.XLeft,
+            Vec3Rotation.YLeft,
+            Vec3Rotation.ZLeft,
+            Vec3Rotation.XRight,
+            Vec3Rotation.YRight,
+            Vec3Rotation.ZRight,
+            Vec3Rotation.X180,
+            Vec3Rotation.Y180,
+            Vec3Rotation.Z180,
+        };
+
+        Random m_rand= new Random();
+
+        public (bool, Vec3Rotation, Vec3) TryMatch(Scanner a, int i, Scanner b, int j)
+        {
+            Vec3 beacon1 = a.Beacons[i];
+            Vec3 beacon2 = a.Beacons[j];
+
+            Vec3Rotation rot = Transforms[m_rand.Next(Transforms.Length)];
+            Vec3 beacon2Rot = beacon2 * rot;
+
+            Vec3 trans = beacon1 - beacon2;
+
+            int match = 0;
+            foreach(Vec3 testBeacon in b.Beacons)
+            {
+                var test = testBeacon * rot;
+                test = test + trans;
+                if (a.Beacons.Count(x => x.Equals(test)) > 0)
+                    match++;
+            }
+
+            if (match >= 12)
+                return (true, rot, trans);
+            else
+                return (false, Vec3Rotation.None, Vec3.Zero);
+        }
+
+        public (bool, Vec3Rotation, Vec3) TryMatch(Scanner a, Scanner b)
+        {
+            int i = m_rand.Next(a.Beacons.Count);
+            int j = m_rand.Next(b.Beacons.Count);
+            return TryMatch(a, i, b, j);
+        }
+
+        public override string Part1()
+        {
+            List<Scanner> matched = new();
+            List<Scanner> waiting = new(m_scanners);
+
+            // Start with the first scanner in the list
+            matched.Add(waiting.First());
+            waiting.RemoveAt(0);
+
+            while(true)
+            {
+                int a = m_rand.Next(matched.Count);
+                int b = m_rand.Next(waiting.Count);
+
+                var scannerA = matched[a];
+                var scannerB = waiting[b];
+                    
+                var overlap = scannerA.Distances.Intersect(scannerB.Distances);
+                if(overlap.Count() >= 66)
+                {
+                    (var success, var rot, var trans) = TryMatch(scannerA, scannerB);
+                }
+            }
+        }
+
+        public override string Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
