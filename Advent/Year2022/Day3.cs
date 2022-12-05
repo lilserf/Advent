e98d71aef379962b63d6cebe7dc9962d39e13cda
Mark Haines <lilserf@gmail.com>
2022-12-04 22:16:05 -0500

    2022 Day 3 and 4

---
+++ Advent/Year2022/Day3.cs
@@ -0,0 +1,60 @@
+﻿using Advent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2020.Year2022
+{
+
+    internal class Day3 : DayLineLoaderBasic
+    {
+        List<string> m_lines = new();
+
+        public override void ParseInputLine(string line)
+        {
+            m_lines.Add(line);
+        }
+
+        public override string Part1()
+        {
+            int sum = 0;
+            foreach(string line in m_lines)
+            {
+                string part1 = line.Substring(0, line.Length / 2);
+                string part2 = line.Substring(line.Length / 2);
+
+                var common = part1.Intersect(part2).First();
+                var prio = GetPrio(common);
+                sum += prio;
+            }
+
+            return sum.ToString();
+        }
+
+        int GetPrio(char common)
+        {
+            return (common >= 'a' && common <= 'z') ? common - 'a' + 1 : common - 'A' + 27;
+        }
+
+        public override string Part2()
+        {
+            int i = 0;
+            int sum = 0;
+            while(i < m_lines.Count)
+            {
+                var l1 = m_lines[i];
+                var l2 = m_lines[i + 1];
+                var l3 = m_lines[i + 2];
+
+                var badge = l1.Intersect(l2).Intersect(l3).First();
+                sum += GetPrio(badge);
+
+                i += 3;
+            }
+
+            return sum.ToString();
+        }
+    }
+}
---
+++ Advent/Year2022/Day4.cs
@@ -0,0 +1,55 @@
+﻿using Advent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2020.Year2022
+{
+    struct Span
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public Span(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+        public bool IsInsideOther(Span other)
+        {
+            return (Start >= other.Start && End <= other.End);
+        }
+
+        public bool Overlaps(Span other)
+        {
+            return !(other.End < Start || other.Start > End);
+        }
+    }
+
+    internal class Day4 : DayLineLoaderBasic
+    {
+        List<(Span, Span)> m_spans = new();
+        public override void ParseInputLine(string line)
+        {
+            var split = line.Split(',');
+            var first = split[0].Split('-');
+            var second = split[1].Split('-');
+
+            var span1 = new Span(int.Parse(first[0]), int.Parse(first[1]));
+            var span2 = new Span(int.Parse(second[0]), int.Parse(second[1]));
+            m_spans.Add((span1, span2));
+        }
+
+        public override string Part1()
+        {
+            return m_spans.Where(x => x.Item1.IsInsideOther(x.Item2) || x.Item2.IsInsideOther(x.Item1)).Count().ToString();
+        }
+
+        public override string Part2()
+        {
+            return m_spans.Where(x => x.Item1.Overlaps(x.Item2)).Count().ToString();
+        }
+    }
+}
