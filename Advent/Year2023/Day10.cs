e799a78422aae7ea3a8a6df1cdd309d688b6715d
Mark Haines <lilserf@gmail.com>
2023-12-19 16:35:27 -0500

    2023 Day 10 part 1 and Day 11 (both parts)

---
+++ Advent/Year2023/Day10.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Year2023
+{
+    internal class Day10 : DayLineLoaderBasic
+    {
+        Dictionary<Vec2, char> m_map = new();
+        Dictionary<Vec2, int> m_distance = new();
+        Vec2 m_start;
+
+        public override void ParseInputLine(string line, int lineNum)
+        {
+            int col = 0;
+            foreach (char c in line)
+            {
+                var pos = new Vec2(col, lineNum);
+                m_map[pos] = c;
+                if (c == 'S')
+                    m_start = pos;
+                col++;
+            }
+        }
+
+
+        Dictionary<char, (Vec2, Vec2)> TILES = new ()
+        {
+            { '|', (Vec2.Up, Vec2.Down) },
+            { '-', (Vec2.Left, Vec2.Right) },
+            { 'L', (Vec2.Up, Vec2.Right) },
+            { 'J', (Vec2.Up, Vec2.Left) },
+            { '7', (Vec2.Down, Vec2.Left) },
+            { 'F', (Vec2.Down, Vec2.Right) },
+        };
+
+
+        Vec2 MoveThrough(Vec2 position, Vec2 prevPosition)
+        {
+            var edge = prevPosition - position;
+            char c = m_map[position];
+
+            var choose = (Vec2 edge, Vec2 first, Vec2 second) =>
+            {
+                if (edge == first)
+                    return position + second;
+                else
+                    return position + first;
+            };
+
+            var dirs = TILES[c];
+            return choose(edge, dirs.Item1, dirs.Item2);
+        }
+
+        public override string Part1()
+        {
+            m_distance[m_start] = 0;
+
+            Queue<(Vec2, Vec2)> states = new();
+
+            foreach (var pos in Vec2.CardinalAdjacent)
+            {
+                var check = pos + m_start;
+
+                if (m_map.ContainsKey(check) && m_map[check] != '.')
+                {
+                    var diff = m_start - check;
+                    var dirs = TILES[m_map[check]];
+
+                    if (dirs.Item1 == diff || dirs.Item2 == diff)
+                    {
+                        states.Enqueue((check, m_start));
+                        m_distance[check] = 1;
+                    }
+                }
+            }
+
+
+            while(true)
+            {
+                var state = states.Dequeue();
+
+                var newPos = MoveThrough(state.Item1, state.Item2);
+                Console.WriteLine($"Move from {state.Item2} to {state.Item1} ends up in {newPos}");
+
+                if(m_distance.ContainsKey(newPos))
+                    break;
+                m_distance[newPos] = m_distance[state.Item1] + 1;
+                Console.WriteLine($"  Distance is {m_distance[newPos]}");
+
+                state.Item2 = state.Item1;
+                state.Item1 = newPos;
+                states.Enqueue(state);
+            }
+
+            return m_distance.Values.Max().ToString();
+        }
+
+        public override string Part2()
+        {
+            // Ugh I don't wanna right now
+            return "";
+        }
+    }
+}
---
+++ Advent/Year2023/Day11.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Year2023
+{
+    internal class Day11 : DayLineLoaderBasic
+    {
+        List<string> m_lines = new();
+
+        string m_columns = string.Empty;
+        List<int> m_columnJumps = new();
+        List<int> m_rowJumps = new();
+        Dictionary<int, Vec2> m_galaxies = new();
+
+        public override void ParseInputLine(string line, int lineNum)
+        {
+            // First add the line
+            if(!line.Contains('#'))
+            {
+                m_rowJumps.Add(lineNum);
+            }
+
+            m_lines.Add(line);
+
+            // Then OR the columns
+            if (m_columns == string.Empty)
+            {
+                m_columns = line;
+            }
+            else
+            {
+                m_columns = string.Join("", m_columns.Zip(line, (a, b) =>
+                {
+                    if (a == '#' || b == '#')
+                        return '#';
+                    else
+                        return '.';
+                }));
+            }
+
+        }
+
+
+        public override void ParseInputLinesEnd(StreamReader sr)
+        {
+            List<int> indices = new();
+
+            for(int i=0; i < m_columns.Length; i++)
+            {
+                if (m_columns[i] == '.')
+                    m_columnJumps.Add(i);
+            }
+
+            int galNum = 1;
+            for(int i=0; i < m_lines.Count; i++)
+            {
+                for(int j=0; j < m_lines[i].Length; j++)
+                {
+                    if (m_lines[i][j] == '#')
+                    {
+                        m_galaxies.Add(galNum, new Vec2(i, j));
+                        galNum++;
+                    }
+                }
+            }
+        }
+
+        int NumJumps(Vec2 a, Vec2 b)
+        {
+            int rows = m_rowJumps.Count(x => (a.X < x && x < b.X) || (b.X < x && x < a.X));
+            int cols = m_columnJumps.Count(y => (a.Y < y && y < b.Y) || (b.Y < y && y < a.Y));
+
+            return rows + cols;
+        }
+
+        public long DoWork(long expandValue)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < m_galaxies.Count; i++)
+            {
+                var gal1 = m_galaxies.ElementAt(i);
+                for (int j = i; j < m_galaxies.Count; j++)
+                {
+                    var gal2 = m_galaxies.ElementAt(j);
+
+                    int dist = Vec2.ManhattanDistance(gal1.Value, gal2.Value);
+
+                    //Console.WriteLine($"G{gal1.Key} => G{gal2.Key}: {dist} squares");
+
+                    int jumps = NumJumps(gal1.Value, gal2.Value);
+                    //Console.WriteLine($"  {jumps} jumps found");
+
+                    sum += dist + (jumps * (expandValue-1));
+                }
+            }
+
+            return sum;
+        }
+
+        public override string Part1()
+        {
+            var sum = DoWork(2);
+
+            return $"{sum}";
+        }
+
+        public override string Part2()
+        {
+            var sum = DoWork(1000000);
+            return $"{sum}";
+        }
+    }
+}
